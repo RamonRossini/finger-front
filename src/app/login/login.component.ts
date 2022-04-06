57e888efc3226ebe3b32d6f0ce7b6ce23746dc1c
RamonRossini <ramonhbonfim@gmail.com>
2022-04-06 00:46:03 -0300

    Funcao de login e criado home component

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { LoginService } from './login.service';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +10,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  formLogin: FormGroup = this.formBuilder.group({
+    email: ['', Validators.required],
+    password: ['', Validators.required]
+  })
+
+  constructor(
+    private readonly loginService: LoginService,
+    private readonly formBuilder: FormBuilder,
+    private readonly router: Router
+  ) { }
 
   ngOnInit(): void {
+    
+  }
+
+  login() {
+    if (this.formLogin.valid) {
+      const form = this.formLogin.value;
+
+      this.loginService.login(form).subscribe(res => {
+        if (res) {
+          this.router.navigate(['/home']);
+        } else {
+          console.log('não encontrado');
+          
+        }
+      })
+    } else {
+      console.log('form invalido');
+      
+    }
   }
 
 }
--- src/app/login/login.service.ts
+++ src/app/login/login.service.ts
@@ -1,9 +1,18 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LoginService {
 
-  constructor() { }
+  constructor(
+    private readonly http: HttpClient
+  ) { }
+
+  login(form: any) {
+    return this.http.post(`${environment.api_url}/users/login`, form);
+  }
+
 }
